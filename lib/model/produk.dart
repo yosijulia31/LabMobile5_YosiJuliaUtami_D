665dba8291f4e3518350c30766b0c8bb0ca55c1d
yosijulia31 <yosi.utami31@gmail.com>
2024-10-10 00:12:33 +0700

    first commit

---
+++ lib/model/produk.dart
@@ -0,0 +1,14 @@
+class Produk {
+String? id;
+String? kodeProduk;
+String? namaProduk;
+var hargaProduk;
+Produk({this.id, this.kodeProduk, this.namaProduk, this.hargaProduk});
+factory Produk.fromJson(Map<String, dynamic> obj) {
+return Produk(
+id: obj['id'],
+kodeProduk: obj['kode_produk'],
+namaProduk: obj['nama_produk'],
+hargaProduk: obj['harga']);
+}
+}
